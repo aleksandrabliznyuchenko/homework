e19299470eb5ee06599fa862797ce2d3e7e4cc87
aleksandrabliznyuchenko <sb0408@mail.ru>
2016-10-17 16:40:06 +0300

    Add files via upload

---
+++ hw1_python.py
@@ -0,0 +1,13 @@
+a = int(input('a: '))
+b = int(input('b: '))
+c = int(input('c: '))
+
+if a+b == c:
+    print ('True')
+else:
+    print ('False')
+
+if a//b == c:
+    print ('True')
+else:
+    print ('False')
