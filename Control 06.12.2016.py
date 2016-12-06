7a3f4d487666b844a9352f886f23f4fb100e4809
aleksandrabliznyuchenko <sb0408@mail.ru>
2016-12-06 14:58:45 +0300

    Update Control 06.12.2016.py

--- Control 06.12.2016.py
+++ Control 06.12.2016.py
@@ -5,8 +5,8 @@ with open('Test 1.txt', 'r', encoding = 'utf-8') as f:
             print(*lines)
 
 quote = 0           
-with open('Test 1.txt', 'r', encoding = 'utf-8') as f:
-    for line in f:
+with open('Test 1.txt', 'r', encoding = 'utf-8') as t:
+    for line in t:
         lines = line.split()        
         for elem in lines:
             if elem == 'разум':
