eeed698b1c7f37af1a70b8b8278b2a1681df4dc4
aleksandrabliznyuchenko <sb0408@mail.ru>
2016-12-06 14:56:57 +0300

    Add files via upload

---
+++ Control 06.12.2016.py
@@ -0,0 +1,14 @@
+with open('Test 1.txt', 'r', encoding = 'utf-8') as f:
+    for line in f:
+        lines = line.split()
+        if len(lines) < 10:
+            print(*lines)
+
+quote = 0           
+with open('Test 1.txt', 'r', encoding = 'utf-8') as f:
+    for line in f:
+        lines = line.split()        
+        for elem in lines:
+            if elem == 'разум':
+                quote += 1
+    print(quote)
