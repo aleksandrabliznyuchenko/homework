c25d8fd1265f443a91759615adcdd65bcf2fe615
aleksandrabliznyuchenko <sb0408@mail.ru>
2016-12-06 09:20:04 +0300

    Update hw5_4.py

--- python_hw/hw5_4.py
+++ python_hw/hw5_4.py
@@ -12,5 +12,9 @@ with open('Hw5.txt', 'r', encoding ='utf-8') as f:
                 quantity += 1
 
 print(quantity * 100//words)
-                    
+# Сейчас программа считает процент слов, оканчивающихся знаком препинания. Для правильного результата:
+# sign = quantity * 100//words
+# result = 100 - int(sign)
+# print(result)
+# Но это дошло до меня уже после дедлайна
                
