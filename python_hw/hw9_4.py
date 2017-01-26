2dd5c2924142c2375e32a3acd50d1ae7a0c2c1c3
aleksandrabliznyuchenko <sb0408@mail.ru>
2017-01-26 19:27:26 +0300

    Update hw9_4.py

--- python_hw/hw9_4.py
+++ python_hw/hw9_4.py
@@ -7,7 +7,8 @@ with open('karenina.txt', 'r', encoding = 'utf-8') as f:
              if word != ' ']
     forms = []
     for elem in words:
-        m = re.search('(вып(ь(ю(т?)|е(т(е?)м|шь))|и(в(ш?и?й?)|т(ь?|ы?й?)|л(а?|и?|о?))|ей(т?е?)))', elem)
+        m = re.search('(вып(ь(ю(т?)|е(т(е?)м|шь))|и(в(ш?и?й?)|'
+                      'т(ь?|ы?(й?|е?|х?|м?и?)|а?я?|о?(г?о?|й?|м?у?)|у?ю?)|л(а?|и?|о?))|ей(т?е?)))', elem)
         if m != None:
             if elem not in forms:
                 forms.append(elem)
