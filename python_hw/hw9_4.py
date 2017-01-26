0921c4148ee4919eee5c93455578450515ec949f
aleksandrabliznyuchenko <sb0408@mail.ru>
2017-01-26 16:23:26 +0300

    Add files via upload

---
+++ python_hw/hw9_4.py
@@ -0,0 +1,18 @@
+import re
+with open('karenina.txt', 'r', encoding = 'utf-8') as f:
+    text = f.read().replace('\n', ' ').lower() 
+    words = text.split()
+    punct = '.,!?:;-_()""\''
+    words = [word.strip(punct) for word in words
+             if word != ' ']
+    forms = []
+    for elem in words:
+        m = re.search('(вып(ь(ю(т?)|е(т(е?)м|шь))|и(в(ш?и?й?)|т(ь?|ы?й?)|л(а?|и?|о?))|ей(т?е?)))', elem)
+        if m != None:
+            if elem not in forms:
+                forms.append(elem)
+    for elem in forms:
+        print(elem)
+
+
+
