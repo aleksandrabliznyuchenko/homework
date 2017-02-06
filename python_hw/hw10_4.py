0181c1db46dad97fc0a4a2ee0a34c7c0c7dc6c42
aleksandrabliznyuchenko <sb0408@mail.ru>
2017-02-06 19:43:58 +0300

    Update hw10_4.py

--- python_hw/hw10_4.py
+++ python_hw/hw10_4.py
@@ -1,10 +1,18 @@
-import re
 with open('lomonosov.html', 'r', encoding = 'utf-8') as f1:
     text = f1.read()
-    lines = text.split('\n', 20)
-    science = lines[19]
-with open('science.txt', 'w', encoding = 'utf-8') as f2:
-    f2.write(science)
+    lines = text.split('\n')
+    number = 17
+    while number != 0:
+        line = lines[number]
+        if 'Научная' in line:
+            sphere = lines[number + 1]
+            break
+        else:
+            number += 1
+            continue
+    
+with open('sphere.txt', 'w', encoding = 'utf-8') as f2:
+    f2.write(sphere)
    
     
 
