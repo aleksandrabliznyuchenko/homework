6447e56256f0cd5db259f3b3662991b49af40d61
aleksandrabliznyuchenko <sb0408@mail.ru>
2017-11-17 21:11:03 +0300

    Flask intro
    Программа, позволяющая узнать ISO-код языка (её начало)

--- Everything from seminars/ISO-code.py
+++ Everything from seminars/ISO-code.py
@@ -4,5 +4,5 @@ with open ('lang.tsv', 'r', encoding = 'utf-8') as f:
     for lines in f:
         elems = lines.split('\t')
         codes[elems[0]] = elems[1:5]
-        for value in codes.values():
-            print(value)
+        ##for value in codes.values():
+            ##print(value)
