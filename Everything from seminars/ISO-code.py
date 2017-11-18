f319f52f219bdfcf8e90d62df2455425ee20acd8
aleksandrabliznyuchenko <sb0408@mail.ru>
2017-11-18 13:17:17 +0300

    Flask intro
    Пользователь вводит в форму название языка, а программа выдаёт его (языка) ISO-код. Код берётся из скопированной таблицы из Википедии (формат .tsv)

---
+++ Everything from seminars/ISO-code.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+codes = {}
+
+with open ('lang.tsv', 'r', encoding = 'utf-8') as f:
+    for lines in f:
+        elems = lines.split('\t')
+        codes[elems[0]] = elems[1:4]
+
+
+@app.route('/language')
+def language():
+    if request.args['lang'] != None:
+        #lang = request.args['lang']
+        language = codes[request.args['lang']]
+        code = ', '.join(language)
+        return 'ISO-код языка: ' + code
+    else:
+        print('None')
+
+if __name__ == '__main__':
+    app.run(debug=True)
